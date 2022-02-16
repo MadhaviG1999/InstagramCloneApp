96ab76f73371914d41610d81cf61bc093596af02
MadhaviG1999 <93818459+MadhaviG1999@users.noreply.github.com>
2022-02-16 18:12:53 +0530

    Add files via upload

---
+++ Back-End/Instagram-status/main/java/com/example/Instagrampost/test/InstagramStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Instagramstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
